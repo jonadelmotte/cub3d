3b3030ebc40960665eaa7e32024043de9276dddd
Soraya DABBAS <sdabbas@f1r4s18.paris.42.school>
2026-08-31 16:28:06 +0200

    lala

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: sdabbas <sdabbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 15:14:28 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/08/31 16:14:24 by sdabbas          ###   ########.fr       */
+/*   Updated: 2026/08/31 16:18:53 by sdabbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,24 @@
 
 # include "libft/libft.h"
 // # include "mlx.h"
+
+# define EMPTY_SPACE '0'
+# define WALL '1'
+# define ESC 65307
+# define KEY_W 119
+# define KEY_S 115
+# define KEY_A 97
+# define KEY_D 100
+# define UP 65362
+# define DOWN 65364
+# define LEFT 65361
+# define RIGHT 65363
+
+typedef struct s_cub
+{
+    char **map;
+    int width;
+    int height;
+}           t_cub;
 
 #endif
